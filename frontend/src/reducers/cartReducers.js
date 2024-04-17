0de4c91a4c8c88746379bf7dfab5f981bdba5096
cagatayiscann <cagatayiscan@gmail.com>
2024-04-17 17:32:47 +0300

    commit

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,56 @@
+import { 
+    CART_ADD_ITEM,
+    CART_REMOVE_ITEM,
+    CART_SAVE_SHIPPING_ADDRESS,
+     
+    CART_SAVE_PAYMENT_METHOD 
+} from '../constants/cartConstants'
+
+
+export const cartReducer = ( state = { cartItems: [], shippingAddress:{}}, action) => {
+    switch(action.type){
+        case CART_ADD_ITEM:
+            const item = action.payload
+            const existItem = state.cartItems.find(x => x.product === item.product)
+
+            if(existItem){
+                return{
+                    ...state,
+                    cartItems: state.cartItems.map(x => 
+                        x.product === existItem.product ? item : x)
+                }
+            }else{
+                return{
+                    ...state,
+                    cartItems: [...state.cartItems, item]
+                }
+            }
+
+
+            case CART_REMOVE_ITEM:
+                return{
+                    ...state,
+                    cartItems:state.cartItems.filter(x => x.product !== action.payload)
+                //mapping through. x.product which is the product id is not equal to action.payload
+                //in our payload we are gonna send an id. the id we wanna remove.
+                //filter is gonna keep every product that doesn't match with payload id.
+                //and we get back a new array.
+                
+                }
+
+            case CART_SAVE_SHIPPING_ADDRESS:
+                return{
+                    ...state,
+                    shippingAddress: action.payload
+                }
+
+            case CART_SAVE_PAYMENT_METHOD:
+                return{
+                    ...state,
+                    paymentMethod: action.payload
+                }
+
+        default:
+            return state
+    }
+}
